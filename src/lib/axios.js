25f876c64e73e464ea4d2e32267fc6b93f18407b
VernPherus <vernpherus@gmail.com>
2026-02-23 14:15:43 +0800

    PATCH: Fixed wrong route

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create axios instance with default config
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
